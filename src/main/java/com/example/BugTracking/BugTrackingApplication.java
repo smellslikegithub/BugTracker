0534b741eada8232277630f7339359601d68320d
smellslikegithub <ashish.m.husain@gmail.com>
2020-04-29 21:45:20 +0200

    initialied a spring-boot app

---
+++ src/main/java/com/example/BugTracking/BugTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.BugTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BugTracking/BugTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BugTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
